f47e3467ed9ec94be9d501917e1996195a379e5c
Davis Gossage <dcg13@duke.edu>
2016-04-13 17:42:16 -0400

    Part 2

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -5,10 +5,25 @@ angular.module('myApp.view1', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'View1Ctrl as store'
   });
 }])
 
 .controller('View1Ctrl', [function() {
+  this.products = [{
+    name: "Macbook Air",
+    description: "11 inch notebook",
+    price: 799,
+    canPurchase: false
+  },
+  {
+    name: "Macbook Pro",
+    description: "13 inch notebook",
+    price: 1099,
+    canPurchase: false
+  }]
 
+  this.buy = function(product){
+    console.log(product)
+  }
 }]);
